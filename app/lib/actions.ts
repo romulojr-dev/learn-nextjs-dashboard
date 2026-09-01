2d7d355f7e6566448d263638c4f0f10e1bf6245c
romulojr-dev <romulo.dev@yahoo.com>
2026-09-01 21:49:14 +0800

    minor bugfix: delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -110,7 +110,7 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
  
   // Unreachable code block
   await sql`DELETE FROM invoices WHERE id = ${id}`;
